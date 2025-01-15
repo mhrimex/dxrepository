a93e7390429dd1ee22b70bf2bb4ca998e5b1658f
developer8 <mohamadhashem.rimex@gmail.com>
2025-01-15 11:38:29 +0200

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+$(function() {
+    const dataSource = [
+      { id: 1, name: "John Doe", age: 29 },
+      { id: 2, name: "Jane Smith", age: 34 },
+      { id: 3, name: "Sam Johnson", age: 45 }
+    ];
+  
+    $("#gridContainer").dxDataGrid({
+      dataSource: dataSource,
+      columns: [
+        { dataField: "id", caption: "ID", width: 50 },
+        { dataField: "name", caption: "Name" },
+        { dataField: "age", caption: "Age", width: 50 }
+      ],
+      showBorders: true
+    });
+  });
+  
